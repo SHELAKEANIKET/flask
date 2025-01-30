f5b7c4a9b11653f2b3b2ea9c8fa1183ac6df6c22
Aniket Shelake <aniketshelake151@sitcoe.org.in>
2025-01-30 20:33:50 +0530

    update code

--- inventory_management/app/__init__.py
+++ inventory_management/app/__init__.py
@@ -8,10 +8,10 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = ''
-    app.config['MYSQL_DB'] = 'inventory_management'
+    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
     
     app.secret_key = os.getenv("SECRET_KEY")
 
